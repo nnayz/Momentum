9d5460957e78fc944c2cc764d10ba290e51e61b2
Nasrul Huda <nasrul2001@gmail.com>
2025-06-30 11:29:18 +0200

    pyproject.toml

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,11 @@ def read_root():
     return {"message": "Hello, World!"}
 
 
+@app.post('/new-folder')
+def new_folder(folder_name: str):
+    return {"message": "Folder created successfully", "folder_name": folder_name}
+
+
 @app.post("/import-document")
 def import_document(file: UploadFile = File(...)):
     
